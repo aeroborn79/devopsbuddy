b1e6951091a7dd210855c4d92fc450ff17791dea
aeroborn79 <aeroborn79@yahoo.com>
2018-02-25 22:35:02 -0600

    Set up the H2 console and created the repository layer

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -3,15 +3,12 @@ package com.devopsbuddy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import com.devopsbuddy.web.i18n.I18NService;
 import junit.framework.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
 @WebAppConfiguration
 public class DevopsbuddyApplicationTests {
 
